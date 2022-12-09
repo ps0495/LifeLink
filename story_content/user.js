f94c1f74c47f654dcc80dc178a89a13d3510d77d
Pawan <pw040495@gmail.com>
2022-12-09 22:50:14 +0530

    WBT

---
+++ story_content/user.js
@@ -0,0 +1,375 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67sxEDFKpro":
+        Script1();
+        break;
+      case "69huiV7um24":
+        Script2();
+        break;
+      case "5uccLVMUyxf":
+        Script3();
+        break;
+      case "6219xGcQiA9":
+        Script4();
+        break;
+      case "67JHe8GEGFr":
+        Script5();
+        break;
+      case "5zf2On4BjtQ":
+        Script6();
+        break;
+      case "5ey5D09bcpD":
+        Script7();
+        break;
+      case "6m9rFhHvQWX":
+        Script8();
+        break;
+      case "65Cbg1RQj8X":
+        Script9();
+        break;
+      case "5z6gTPOsqrW":
+        Script10();
+        break;
+      case "5xlGuvxcqhy":
+        Script11();
+        break;
+      case "5nRSU9sB1oY":
+        Script12();
+        break;
+      case "6LeIYExbKk5":
+        Script13();
+        break;
+      case "6ipSuI4rrVK":
+        Script14();
+        break;
+      case "5wq8TwLnn6T":
+        Script15();
+        break;
+      case "65xF51y4udI":
+        Script16();
+        break;
+      case "6d4kxTlKmyj":
+        Script17();
+        break;
+      case "6eh7sRfzTio":
+        Script18();
+        break;
+      case "6igyO1Ydo8c":
+        Script19();
+        break;
+      case "6Ms1Zz9bgqJ":
+        Script20();
+        break;
+      case "5yvYpXz4GKh":
+        Script21();
+        break;
+      case "5lYgS5mUy4S":
+        Script22();
+        break;
+      case "5laXK51mDsS":
+        Script23();
+        break;
+      case "5nMZuLKHcwu":
+        Script24();
+        break;
+      case "5yaVnGPMQWJ":
+        Script25();
+        break;
+      case "5sLx6wKamqa":
+        Script26();
+        break;
+      case "6J4BiqTDMNa":
+        Script27();
+        break;
+      case "6HaBE0MfiuM":
+        Script28();
+        break;
+      case "65kyBtW0qHk":
+        Script29();
+        break;
+      case "5akK3vrxi2B":
+        Script30();
+        break;
+      case "6LeRn9WRFXU":
+        Script31();
+        break;
+      case "6D44Jsno10G":
+        Script32();
+        break;
+      case "6ESbLWHYkq9":
+        Script33();
+        break;
+      case "69p26BM4aj7":
+        Script34();
+        break;
+      case "68KCSNfOu51":
+        Script35();
+        break;
+      case "5ptrLwdRceR":
+        Script36();
+        break;
+      case "6gVY7zUXKgW":
+        Script37();
+        break;
+      case "5onV7haKYRM":
+        Script38();
+        break;
+      case "6Jiy0OCTeu4":
+        Script39();
+        break;
+      case "6kippRtZR0h":
+        Script40();
+        break;
+      case "6okG6YH6f2H":
+        Script41();
+        break;
+      case "6L2fMaNgnzF":
+        Script42();
+        break;
+      case "69lg9yCUMAL":
+        Script43();
+        break;
+      case "5X9ROXupSBU":
+        Script44();
+        break;
+      case "636cEngohte":
+        Script45();
+        break;
+      case "66gVg8PCtQh":
+        Script46();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.open.close();
+}
+
+function Script2()
+{
+  window.open.close();
+}
+
+function Script3()
+{
+  window.open.close();
+}
+
+function Script4()
+{
+  window.open.close();
+}
+
+function Script5()
+{
+  window.open.close();
+}
+
+function Script6()
+{
+  window.open.close();
+}
+
+function Script7()
+{
+  window.open.close();
+}
+
+function Script8()
+{
+  window.open.close();
+}
+
+function Script9()
+{
+  window.open.close();
+}
+
+function Script10()
+{
+  window.open.close();
+}
+
+function Script11()
+{
+  window.open.close();
+}
+
+function Script12()
+{
+  window.open.close();
+}
+
+function Script13()
+{
+  window.open.close();
+}
+
+function Script14()
+{
+  window.open.close();
+}
+
+function Script15()
+{
+  window.open.close();
+}
+
+function Script16()
+{
+  window.open.close();
+}
+
+function Script17()
+{
+  window.open.close();
+}
+
+function Script18()
+{
+  window.open.close();
+}
+
+function Script19()
+{
+  window.open.close();
+}
+
+function Script20()
+{
+  window.open.close();
+}
+
+function Script21()
+{
+  window.open.close();
+}
+
+function Script22()
+{
+  window.open.close();
+}
+
+function Script23()
+{
+  window.open.close();
+}
+
+function Script24()
+{
+  window.open.close();
+}
+
+function Script25()
+{
+  window.open.close();
+}
+
+function Script26()
+{
+  window.open.close();
+}
+
+function Script27()
+{
+  window.open.close();
+}
+
+function Script28()
+{
+  window.open.close();
+}
+
+function Script29()
+{
+  window.open.close();
+}
+
+function Script30()
+{
+  window.open.close();
+}
+
+function Script31()
+{
+  window.open.close();
+}
+
+function Script32()
+{
+  window.open.close();
+}
+
+function Script33()
+{
+  window.open.close();
+}
+
+function Script34()
+{
+  window.open.close();
+}
+
+function Script35()
+{
+  window.open.close();
+}
+
+function Script36()
+{
+  window.open.close();
+}
+
+function Script37()
+{
+  window.open.close();
+}
+
+function Script38()
+{
+  window.open.close();
+}
+
+function Script39()
+{
+  window.open.close();
+}
+
+function Script40()
+{
+  window.open.close();
+}
+
+function Script41()
+{
+  window.open.close();
+}
+
+function Script42()
+{
+  window.open.close();
+}
+
+function Script43()
+{
+  window.open.close();
+}
+
+function Script44()
+{
+  window.open.close();
+}
+
+function Script45()
+{
+  window.open.close();
+}
+
+function Script46()
+{
+  window.open.close();
+}
+
